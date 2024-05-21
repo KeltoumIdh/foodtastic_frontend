acc1b3b09d93d688cac313e6834a7912b75bd040
hixvm <hixvmx@gmail.com>
2024-05-21 22:40:22 +0100

    fix(/lib/axios.jsx): change baseUrl to 'http://127.0.0.1:8000'

--- src/lib/axios.jsx
+++ src/lib/axios.jsx
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 // Create an Axios instance with the appropriate adapter
 const axios = Axios.create({
-  baseURL: "http://localhost:8000/", //process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "http://127.0.0.1:8000/", //process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
     Accept: "application/json",
